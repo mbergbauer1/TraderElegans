26bb3b9f6ca150452b876d10ddd1963f99ae5020
mbergbauer1 <mbergbauer1@gmail.com>
2017-04-04 18:37:48 -0500

    n/a

--- TraderElegans.py
+++ TraderElegans.py
@@ -1,5 +1,5 @@
 
-import numpy
+import numpy as np
 import math
 from keras.models import Sequential
 from keras.layers import Dense
@@ -11,13 +11,13 @@ from sklearn.metrics import mean_squared_error
 #CLASSES----------------------------------------------------------------------------------------------------------------
 class Constants:
     #Class Variables (static)
-#    file_path = 'C:\\Users\\mbergbauer\\Desktop\\NN\\TraderElegans\\CSV_M1\\'
-    file_path = 'C:\\Users\\bergbmi\Desktop\\NN\\TraderElegans\\Data\\M1_Raw\\'
+    file_path = 'C:\\Users\\mbergbauer\\Desktop\\NN\\TraderElegans\\CSV_M1\\'
+#    file_path = 'C:\\Users\\bergbmi\Desktop\\NN\\TraderElegans\\Data\\M1_Raw\\'
     file_ext = '.csv'
     filename_EURUSD = 'DAT_ASCII_EURUSD_M1_'
     filename_GBPUSD = 'DAT_ASCII_GBPUSD_M1_'
     filename_USDCHF = 'DAT_ASCII_USDCHF_M1_'
-    start_y = 2000
+    start_y = 2015
     end_y = 2017
     end_m = 3
     start_time = 80000
@@ -147,10 +147,15 @@ def read_data(file):
             for item in raw[1:]:
                 raw_prices_tmp.append(float(item))
             raw_date_time.append(raw_date_time_tmp)
-            raw_prices.append(raw_prices_tmp)
+            raw_prices.append(list(raw_prices_tmp))
             del raw_prices_tmp[:]
+        array = np.array(raw_prices)
+        x = len(raw_prices)
+        y = len(raw_prices[0])
+        array.reshape(x,y)
         scaler = MinMaxScaler(feature_range=(0, 1))
-        raw_prices_scaled = scaler.fit_transform(raw_prices).tolist()
+        scaler.fit_transform(np.array(raw_prices))
+        raw_prices_scaled = scaler.fit_transform(np.array(raw_prices)).tolist()
         prevday = ''
         oneDay = None
         for rdt, rps in zip(raw_date_time, raw_prices_scaled):
